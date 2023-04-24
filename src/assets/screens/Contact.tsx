5641ea62ba4ce146ba76eeb5abdc5fca90597c3e
DeschrevelBert <71965636+DeschrevelBert@users.noreply.github.com>
2023-04-24 15:19:48 +0200

    contact page has a form

--- src/assets/screens/Contact.tsx
+++ src/assets/screens/Contact.tsx
@@ -1,12 +1,63 @@
-import Navigation from "../Components/navigation";
+import React from "react";
 
-export default () => {
-    return (
-        <main>
-            <div>
-                <Navigation />
-            </div>
-            
-        </main>
-    );
+const ContactPage = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-3xl font-bold mb-4">Contact Us</h1>
+      <form className="w-full max-w-lg">
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="name"
+          >
+            Name
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="name"
+            type="text"
+            placeholder="John Doe"
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="email"
+          >
+            Email
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="email"
+            type="email"
+            placeholder="johndoe@example.com"
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="message"
+          >
+            Message
+          </label>
+          <textarea
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="message"
+            rows="5"
+            placeholder="Enter your message here"
+          ></textarea>
+        </div>
+        <div className="flex items-center justify-center">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit"
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 };
+
+export default ContactPage;
